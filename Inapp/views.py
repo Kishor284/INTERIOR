0f7bf5abcdcb1afed385442a78b1218c7dfd3116
KISHOR KV <kishorekv284@gmail.com>
2025-04-02 12:35:55 +0530

    Initial commit

--- Inapp/views.py
+++ Inapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+import os
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
